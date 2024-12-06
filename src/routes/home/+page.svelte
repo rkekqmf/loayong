916660853761dcbef1089336007cebf4b6f97d9d
yong-atomrigs <yong@atomrigs.io>
2024-12-06 16:11:42 +0900

    Feature add class icon and add class routing page

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Calculator from './components/calculator.svelte';
 	import Homework from './components/homework.svelte';
+	import ClassInfo from './components/class-info.svelte';
 </script>
 
 <div class="flex flex-col gap-4 md:flex-row">
@@ -10,11 +11,6 @@
 	<div class="flex flex-[1] flex-col gap-4">
 		<Homework />
 
-		<section class="relative flex h-full items-center justify-center gap-9 rounded-xl border-2 border-app-box-border bg-gradient-to-tr from-[#2c402f] to-[#354f34] p-8 shadow-box">
-			<div class="text-center text-white">
-				<h2>Title</h2>
-				<p>description</p>
-			</div>
-		</section>
+		<ClassInfo />
 	</div>
 </div>
