b50c5af74d2d015deb417246cfce9fed4b404ed2
yong-atomrigs <yong@atomrigs.io>
2024-12-20 18:42:07 +0900

    Feature / add class engrave layout

--- src/routes/class/[class]/+page.svelte
+++ src/routes/class/[class]/+page.svelte
@@ -1,18 +1,13 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { classData } from '$lib/data/classData';
+	import Engrave from '../components/engrave.svelte';
 
 	const currentClass = Object.values(classData).find((c) => c.code === $page.params.class);
 </script>
 
-<div>
+<div class="flex items-center gap-4">
 	{#if currentClass}
-		<div class="flex items-center gap-4">
-			<img src="/class/{currentClass.code}.png" alt={currentClass.name} class="h-16 w-16" />
-			<div>
-				<h1>{currentClass.name}</h1>
-				<p>Class Code: {currentClass.code}</p>
-			</div>
-		</div>
+		<Engrave selectedClass={currentClass.name} />
 	{/if}
 </div>
